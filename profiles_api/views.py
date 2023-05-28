19c488590a69a4b93817521b0ee2973e40b36116
Atul Kumar <5877089+atulkumar2@users.noreply.github.com>
2023-05-29 01:47:05 +0530

    Added Viewset to profile API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from profiles_api import serializers
+from rest_framework import viewsets
 
 
 class HelloApiView(APIView):
@@ -62,3 +63,79 @@ class HelloApiView(APIView):
             pk (_type_, optional): _description_. Defaults to None.
         """
         return Response({"method": "DELETE"})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API Viewset
+
+    Args:
+        viewsets (_type_): _description_
+    """
+
+    serializer_class = serializers.HelloSerializer
+
+    def list(self, request):
+        """Return a hello message
+
+        Args:
+            request (_type_): _description_
+        """
+
+        a_viewset = [
+            "Uses actions (list, create, retrieve, update, partial_update)",
+            "Automatically maps to URLS using Routers",
+            "Provides more functionality with new code",
+        ]
+        return Response({"message": "Hello!", "a_viewset": a_viewset})
+
+    def create(self, request):
+        """Create a new hello message
+
+        Args:
+            request (_type_): _description_
+        """
+
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get("name")
+            message = f"Hello {name}"
+            return Response({"message": message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    def retrieve(self, request, pk=None):
+        """Handle getting an object by its ID
+
+        Args:
+            request (_type_): _description_
+            pk (_type_, optional): _description_. Defaults to None.
+        """
+        return Response({'http_method': 'GET'})
+    
+    def update(self, request, pk=None):
+        """Handle updating an object
+
+        Args:
+            request (_type_): _description_
+            pk (_type_, optional): _description_. Defaults to None.
+        """
+        return Response({'http_method': 'PUT'})
+    
+    def partial_update(self, request, pk=None):
+        """Handle updating part of an object
+
+        Args:
+            request (_type_): _description_
+            pk (_type_, optional): _description_. Defaults to None.
+        """
+        return Response({'http_method': 'PATCH'})
+    
+    def destroy(self, request, pk=None):
+        """Removing an object
+
+        Args:
+            request (_type_): _description_
+            pk (_type_, optional): _description_. Defaults to None.
+        """
+        return Response({'http_method': 'DELETE'})
